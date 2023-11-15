5e67a3c9c8caf7cfdcdfb1182854de8368e77bae
JasonP2711 <cthang2711@gmail.com>
2023-11-15 21:45:15 +0700

    update 15/11

--- frontEnd Nextjs/src/pages/index.tsx
+++ frontEnd Nextjs/src/pages/index.tsx
@@ -1,6 +1,5 @@
-import { useEffect } from "react";
 import { axiosClient } from "@/libraries/axiosClient";
-import axios from "axios";
+// import { Element, scroller } from "react-scroll";
 import { URL_ENV } from "@/constant/URL";
 import Image from "next/image";
 import styles from "./page.module.css";
@@ -18,6 +17,7 @@ import Brand from "../components/brand/page";
 import Slider from "../components/slider/page";
 import CardMember from "../components/CardMember/page";
 import Quality from "../components/Quality/index";
+import { useEffect, useState } from "react";
 
 type Props = {
   data: any;
@@ -28,7 +28,31 @@ type Props = {
 // const URL_ENV = "http://localhost:9000" || process.env.API_BE_URL;
 
 export default function Home({ data, staff }: Props) {
+  const [scrollY, setScrollY] = useState<any>();
+  const [scrollYSlider, setScrollYSlider] = useState<boolean>(false);
+  const [scrollYlove, setScrollYlove] = useState<boolean>(false);
+  const [scrollYmember, setScrollYmember] = useState<boolean>(false);
+
   const router = useRouter();
+  const handleScroll = () => {
+    setScrollY(window.scrollY);
+    console.log("scroll: ", window.scrollY);
+    if (window.scrollY >= 250 && scrollYSlider === false) {
+      setScrollYSlider(true);
+    }
+    if (window.scrollY >= 700 && scrollYlove === false) {
+      setScrollYlove(true);
+    }
+    if (window.scrollY >= 1500 && scrollYmember === false) {
+      setScrollYmember(true);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <>
       <div className={styles.container}>
@@ -48,11 +72,15 @@ export default function Home({ data, staff }: Props) {
         <div className={styles.title}>
           <span className={styles.title1}>Welcome to TiệmẢnh studio</span>
         </div>
-
         <div className={styles.slider}>
-          <Slider Slidedata={data} />
+          {scrollYSlider === true && (
+            <>
+              <div className={styles.showSlide}>
+                <Slider Slidedata={data} />
+              </div>
+            </>
+          )}
         </div>
-
         <div className={styles.title__2}>
           <p style={{ width: "70%", textAlign: "center" }}>
             Luôn luôn lắng nghe sự góp ý của quý khách để tự hoàn thiện mình,
@@ -64,82 +92,97 @@ export default function Home({ data, staff }: Props) {
           <div className="d-flex justify-content-center">
             <h2>Thể loại được yếu thích</h2>
           </div>
-          <div className={styles.imgPackage1}>
-            <Image
-              src={Package1}
-              alt="My Image"
-              width={450}
-              height={300}
-              onClick={() =>
-                router.push("/photoPackage/6485c0365570ad344f1a9084")
-              }
-            />
-          </div>
+          {scrollYlove === true && (
+            <>
+              <div className={styles.lovePackage}>
+                <div className={styles.imgPackage1}>
+                  <Image
+                    src={Package1}
+                    alt="My Image"
+                    width={450}
+                    height={300}
+                    onClick={() =>
+                      router.push("/photoPackage/6485c0365570ad344f1a9084")
+                    }
+                  />
+                </div>
 
-          <div className={styles.imgPackage2}>
-            <Image
-              src={Package2}
-              alt="My Image"
-              width={300}
-              height={380}
-              onClick={() =>
-                router.push("/photoPackage/6485c07d5570ad344f1a9086")
-              }
-            />
-          </div>
+                <div className={styles.imgPackage2}>
+                  <Image
+                    src={Package2}
+                    alt="My Image"
+                    width={300}
+                    height={380}
+                    onClick={() =>
+                      router.push("/photoPackage/6485c07d5570ad344f1a9086")
+                    }
+                  />
+                </div>
 
-          <div className={styles.imgPackage3}>
-            <Image
-              src={Package3}
-              alt="My Image"
-              width={450}
-              height={300}
-              onClick={() =>
-                router.push("/photoPackage/6481fd73d5638359880f4f58")
-              }
-            />
-          </div>
+                <div className={styles.imgPackage3}>
+                  <Image
+                    src={Package3}
+                    alt="My Image"
+                    width={450}
+                    height={300}
+                    onClick={() =>
+                      router.push("/photoPackage/6481fd73d5638359880f4f58")
+                    }
+                  />
+                </div>
 
-          <div className={styles.imgPackage4}>
-            <Image
-              src={Package4}
-              alt="My Image"
-              width={300}
-              height={380}
-              onClick={() =>
-                router.push("/photoPackage/6485bfb05570ad344f1a9080")
-              }
-            />
-          </div>
+                <div className={styles.imgPackage4}>
+                  <Image
+                    src={Package4}
+                    alt="My Image"
+                    width={300}
+                    height={380}
+                    onClick={() =>
+                      router.push("/photoPackage/6485bfb05570ad344f1a9080")
+                    }
+                  />
+                </div>
 
-          <div className={styles.imgPackage5}>
-            <Image
-              src={Package5}
-              alt="My Image"
-              width={500}
-              height={300}
-              onClick={() =>
-                router.push("/photoPackage/6485c0365570ad344f1a9084")
-              }
-            />
-          </div>
+                <div className={styles.imgPackage5}>
+                  <Image
+                    src={Package5}
+                    alt="My Image"
+                    width={500}
+                    height={300}
+                    onClick={() =>
+                      router.push("/photoPackage/6485c0365570ad344f1a9084")
+                    }
+                  />
+                </div>
 
-          <div className={styles.imgPackage6}>
-            <Image
-              src={Package6}
-              alt="My Image"
-              width={300}
-              height={380}
-              onClick={() =>
-                router.push("/photoPackage/6485c0fc5570ad344f1a9088")
-              }
-            />
-          </div>
+                <div className={styles.imgPackage6}>
+                  <Image
+                    src={Package6}
+                    alt="My Image"
+                    width={300}
+                    height={380}
+                    onClick={() =>
+                      router.push("/photoPackage/6485c0fc5570ad344f1a9088")
+                    }
+                  />
+                </div>
+              </div>
+            </>
+          )}
         </div>
         {/* <h2 className={styles.title__team}>Team</h2> */}
-        <div className={styles.card__member}>
-          <CardMember data={staff?.result} />
+        <div className={styles.memberDiv}>
+          {scrollYmember === true && (
+            <>
+              <div className={styles.memberShow}>
+                <div className={styles.card__member}>
+                  <CardMember data={staff?.result} />
+                </div>
+              </div>
+            </>
+          )}
         </div>
+
         <div className={styles.element}>
           <Quality />
         </div>
