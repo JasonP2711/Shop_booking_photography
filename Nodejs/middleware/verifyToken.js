ca4c27fb97e1a6d194f30d63dec532b966e76cc9
JasonP2711 <cthang2711@gmail.com>
2023-11-18 15:03:19 +0700

    update 18/11

--- Nodejs/middleware/verifyToken.js
+++ Nodejs/middleware/verifyToken.js
@@ -1,11 +1,18 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const token = req.header("token");
+  console.log("here hello anybody here!");
+  const authorizationHeader = req.header["Authorization"];
+  const token = authorizationHeader.split(" ")[1];
+  console.log("token auth: ", token);
   if (!token) return res.status(401).send("Access Denied");
 
   try {
-    const verified = jwt.verify(token, process.env.SECRET);
+    const verified = jwt.verify(token, process.env.SECRET_KEY, (err, data) => {
+      console.log("verify token jwt: ", data, err);
+      if (err) res.sendStatus(403);
+      next();
+    });
     next();
   } catch (err) {
     return res.status(400).send("Invalid Token");
