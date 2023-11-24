bf359eb6e99e05abc89dc63b6e890e865456f29b
JasonP2711 <cthang2711@gmail.com>
2023-11-24 22:18:24 +0700

    update

--- Nodejs/routes/customer.js
+++ Nodejs/routes/customer.js
@@ -73,7 +73,7 @@ router.get(
 // để đơn giản dễ nhớ thì mọi mật khẩu đều là 123456
 router.post(
   "/",
-  cors({ origin: "https://shop-booking-photography.vercel.app/" }),
+  // cors({ origin: "https://shop-booking-photography.vercel.app/" }),
   validateSchema(customerBodySchema),
   async (req, res, next) => {
     try {
