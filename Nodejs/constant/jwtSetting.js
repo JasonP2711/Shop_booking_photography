c6ca72e9387348abb78fcfe672ae0d917e76d236
JasonP2711 <cthang2711@gmail.com>
2023-12-02 21:49:08 +0700

    update 2/12

--- Nodejs/constant/jwtSetting.js
+++ Nodejs/constant/jwtSetting.js
@@ -2,10 +2,4 @@ module.exports = {
   SECRET: process.env.SECRET_KEY,
   AUDIENCE: "aptech.io",
   ISSUER: "softech.cloud",
-  WHITE_LIST: [
-    "tungnt@softech.vn",
-    "peter@gmail.com",
-    "Tom@gmail.com",
-    "bi2@gmail.com",
-  ],
 };
